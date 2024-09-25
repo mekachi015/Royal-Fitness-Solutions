8159d6cdcd767a1334804403199025f1ea29fd9e
mekachi015 <abielmakoti@gmail.com>
2024-09-25 12:58:54 +0200

    methods for routes working

--- Frontend/Royal-Fitness-Solutions/src/app/components/nav-bar/nav-bar.component.ts
+++ Frontend/Royal-Fitness-Solutions/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+
+  constructor( private router: Router){}
+
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
+
+  goToMeals() {
+    this.router.navigate(['/meals']);
+  }
+
+  goToTraining() {
+    this.router.navigate(['/training']);
+  }
+
+  goToContact() {
+    this.router.navigate(['/contact']);
+  }
 
 }
